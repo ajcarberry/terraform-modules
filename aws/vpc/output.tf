908da123a70baae869acbb0eb5d323cce0e9119f
root <root@ip-10-100-255-173.ec2.internal>
2021-02-07 07:34:58 +0000

    supporting tf v0.14

--- aws/vpc/output.tf
+++ aws/vpc/output.tf
@@ -3,13 +3,13 @@
 # =================================
 
 output "vpc_id" {
-  value = "${aws_vpc.vpc_default.id}"
+  value = aws_vpc.vpc_default.id
 }
 
 output "vpc_name" {
-  value = "${aws_vpc.vpc_default.tags.Name}"
+  value = aws_vpc.vpc_default.tags.Name
 }
 
 output "internet_gateway_id" {
-  value = "${aws_internet_gateway.internet_gw_default.id}"
+  value = aws_internet_gateway.internet_gw_default.id
 }
