b39de49176bfa9e89164f6834801862262d993f3
root <root@ip-10-100-255-173.ec2.internal>
2021-02-07 07:23:07 +0000

    supporting tf v0.14

--- aws/subnets/default_subnet/main.tf
+++ aws/subnets/default_subnet/main.tf
@@ -3,22 +3,24 @@
 # NAT Gateway
 # =================================
 resource "aws_nat_gateway" "nat_gw" {
-    subnet_id      = "${var.dmz_subnet}"
-    allocation_id  = "${aws_eip.nat_gw.id}"
+    subnet_id      = var.dmz_subnet
+    allocation_id  = aws_eip.nat_gw.id
 }
 
+# =================================
 # Elastic IP
 # =================================
 resource "aws_eip" "nat_gw" {
   vpc   = true
 }
 
+# =================================
 # Route Table
 # =================================
 # Route table for default subnets
 # =================================
 resource "aws_route_table" "route_nat" {
-  vpc_id = "${var.vpc}"
+  vpc_id = var.vpc
 
   tags = {
     Name = "route_nat_${var.availability_zone}_${var.env}"
@@ -29,19 +31,20 @@ resource "aws_route_table" "route_nat" {
 }
 
 resource "aws_route" "route_rule_nat" {
-  route_table_id = "${aws_route_table.route_nat.id}"
+  route_table_id = aws_route_table.route_nat.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = "${aws_nat_gateway.nat_gw.id}"
+  nat_gateway_id = aws_nat_gateway.nat_gw.id
 }
 
+# =================================
 # Subnet
 # =================================
 # default subnet
 # =================================
 resource "aws_subnet" "subnet_nat" {
-  vpc_id                  = "${var.vpc}"
-  cidr_block              = "${var.cidr}"
-  availability_zone       = "${var.availability_zone}"
+  vpc_id                  = var.vpc
+  cidr_block              = var.cidr
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = false
 
   tags = {
@@ -54,6 +57,6 @@ resource "aws_subnet" "subnet_nat" {
 }
 
 resource "aws_route_table_association" "route_assoc_nat" {
-  subnet_id      = "${aws_subnet.subnet_nat.id}"
-  route_table_id = "${aws_route_table.route_nat.id}"
+  subnet_id      = aws_subnet.subnet_nat.id
+  route_table_id = aws_route_table.route_nat.id
 }
